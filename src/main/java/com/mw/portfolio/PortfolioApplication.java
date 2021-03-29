a008e7439a44e01373dc7be9eef1e4a41763a997
Mitch Warrenburg <mitchwarrenburg@protonmail.com>
2021-03-29 00:50:41 -0700

    Configures properties for both sender and recipient emails.  Includes app user in the intitial email chain by adding a cc to the email.  Sets the reply-to address to the recipient address as opposed to the SendGrid address that sends the intitial email.

--- src/main/java/com/mw/portfolio/PortfolioApplication.java
+++ src/main/java/com/mw/portfolio/PortfolioApplication.java
@@ -3,6 +3,7 @@ package com.mw.portfolio;
 import com.mw.portfolio.config.email.EmailProperties;
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import lombok.val;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
@@ -21,7 +22,13 @@ public class PortfolioApplication {
 
   @EventListener(ApplicationReadyEvent.class)
   public void printEnv() {
-    log.info("--- Application Configuration ---");
-    log.info("[address]: {} [apiKey]: {}", properties.getAddress(), properties.getApiKey());
+    val sender = properties.getSender();
+    val recipient = properties.getRecipient();
+
+    log.info("-------------------------- Application Configuration --------------------------");
+    log.info("[apiKey]: {}", properties.getApiKey());
+    log.info("[senderName]: {} [senderAddress]: {}", sender.getName(), sender.getAddress());
+    log.info("[recipientName]: {} [recipientAddress]: {}", recipient.getName(), recipient.getAddress());
+    log.info("-------------------------------------------------------------------------------");
   }
 }
--- src/main/java/com/mw/portfolio/config/email/EmailProperties.java
+++ src/main/java/com/mw/portfolio/config/email/EmailProperties.java
@@ -1,11 +1,15 @@
 package com.mw.portfolio.config.email;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.annotation.Validated;
 
+import javax.validation.Valid;
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 
 @Data
@@ -16,8 +20,25 @@ import javax.validation.constraints.NotBlank;
 public class EmailProperties {
 
   @NotBlank
-  private String address;
-
-  @NotBlank
   private String apiKey;
+
+  @Valid
+  public EmailDetails sender;
+
+  @Valid
+  public EmailDetails recipient;
+
+  @Data
+  @Builder
+  @Validated
+  @NoArgsConstructor
+  @AllArgsConstructor
+  public static class EmailDetails {
+
+    @NotBlank
+    private String name;
+
+    @Email
+    private String address;
+  }
 }
